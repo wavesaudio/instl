c056186906b02040c8f7f5bc0d531e44ef5b5c18
Shai shasag <shai@waves.com>
2013-01-07 16:10:19 +0200

    copied code from my private repository in bit bucket

---
+++ pyinstl/configVar.py
@@ -0,0 +1,98 @@
+#!/usr/local/bin/python3
+
+from __future__ import print_function
+
+"""
+    Copyright (c) 2012, Shai Shasag
+    All rights reserved.
+    Licensed under BSD 3 clause license, see LICENSE file for details.
+"""
+
+import os
+import sys
+
+
+class ConfigVar(object):
+    """ Keep a single, named, config variable and it's values.
+        Also info about where it came from (file, line).
+        value may have $() style references to other variables.
+        Values are always a list - even a single value is a list of size 1
+        Emulates list container
+    """
+    __slots__ = ("_name", "_description", "_values", "__resolving_in_progress")
+    def __init__(self, name, description="", *values):
+        self._name = name
+        self._description = description
+        self._values = list(values)
+        self.__resolving_in_progress = False # prevent circular resolves
+
+    def name(self):
+        """ return the name of this variable """
+        return self._name
+
+    def description(self):
+        """ return the description of this variable """
+        return self._description
+
+    def set_description(self, description):
+        """ Assign new description """
+        self._description = description
+
+    def __str__(self):
+        ln = os.linesep
+        indent = "    "
+        retVal = "{self._name}:{ln}{indent}description:{self._description}{ln}{indent}values:{self._values})".format(**vars())
+        return retVal
+
+    def __repr__(self):
+        retVal = "{self.__class__.__name__}('{self._name}', '{self._description}',*{self._values})".format(**vars())
+        return retVal
+
+    def __len__(self):
+        return len(self._values)
+
+    def __getitem__(self, key):
+        # if key is of invalid type or value, the list values will raise the error
+        return self._values[key]
+
+    def __setitem__(self, key, value):
+        self._values[key] = value
+
+    def __delitem__(self, key):
+        del self._values[key]
+
+    def __iter__(self):
+        return iter(self._values)
+
+    def __reversed__(self):
+        return reversed(self._values)
+
+    def append(self, value):
+        self._values.append(value)
+
+    def extend(self, value):
+        self._values.extend(value)
+
+class ConstConfigVar(ConfigVar):
+    """ ConfigVar override where values cannot be changed after construction """
+    __slots__ = ()
+    def __init__(self, name, description="", *values):
+        super().__init__(name, description, *values)
+
+    def set_description(self, description):
+        raise Exception("Cannot change a const value", self._name)
+
+    def __setitem__(self, key, value):
+        raise Exception("Cannot change a const value", self._name)
+
+    def __delitem__(self, key):
+        raise Exception("Cannot change a const value", self._name)
+
+    def append(self, value):
+        raise Exception("Cannot change a const value", self._name)
+
+    def extend(self, value):
+        raise Exception("Cannot change a const value", self._name)
+
+if __name__ == "__main__":
+    pass
---
+++ pyinstl/configVarList.py
@@ -0,0 +1,214 @@
+#!/usr/local/bin/python3
+
+from __future__ import print_function
+
+""" 
+    Copyright (c) 2012, Shai Shasag
+    All rights reserved.
+    Licensed under BSD 3 clause license, see LICENSE file for details.
+    
+    configVarList module has but a single class ConfigVarList
+    import config.configVarList
+"""
+
+import os
+import sys
+import re
+
+if __name__ == "__main__":
+    import sys
+    sys.path.append("..")
+
+from config2.configVar import ConfigVar, ConstConfigVar
+
+class ConfigVarList(object):
+    """ Keeps a list of named build config values.
+        Help values resolve $() style references. """
+
+    parser = None
+
+    __slots__ = ("_resolved_vars", "_ConfigVar_objs", "__dirty", "__resolve_stack")
+    def __init__(self):
+        self._resolved_vars = dict()       # map config var name to list of resolved values
+        self._ConfigVar_objs = dict()   # map config var name to list of objects representing unresolved values
+        self.__dirty = False            # True is _ConfigVar_objs was changed but resolved not re-done
+        self.__resolve_stack = None
+
+    def __len__(self):
+        if self.__dirty:
+            raise Exception("config varaibles were not resolved")
+        return len(self._resolved_vars)
+
+    def __getitem__(self, var_name):
+        if self.__dirty:
+            raise Exception("config varaibles were not resolved")
+        # the value of non existant var_name is an empty tuple
+        return self._resolved_vars[var_name]
+
+    def get(self, var_name, default=tuple()):
+        if self.__dirty:
+            raise Exception("config varaibles were not resolved")
+        # the value of non existant var_name is an empty tuple
+        return self._resolved_vars.get(var_name, default)
+
+    def __iter__(self):
+        if self.__dirty:
+            raise Exception("config varaibles were not resolved")
+        return iter(self._resolved_vars)
+
+    def __reversed__(self):
+        if self.__dirty:
+            raise Exception("config varaibles were not resolved")
+        return reversed(self._resolved_vars)
+
+    def __contains__(self, var_name):
+        if self.__dirty:
+            raise Exception("config varaibles were not resolved")
+        if var_name in self._resolved_vars:
+            return True
+        else:
+            return False
+
+    def description(self, var_name):
+        """ Get description for variable """
+        return self._ConfigVar_objs.description(var_name)
+
+    def get_configVar_obj(self, var_name):
+        self.__dirty = True # is someone asked for a ConfigVar, assume it was changed
+        retVal = self._ConfigVar_objs.setdefault(var_name, ConfigVar(var_name))
+        return retVal
+
+    def add_const_config_variable(self, name, description="", *values):
+        """ add a const single value object """
+        self.__dirty = True
+        if in_name in self._ConfigVar_objs:
+            raise Exception("Const variable {name} already defined")
+        addedValue = ConstConfigVar(name, description, *values)
+        self.varDict[addedValue.name()] = addedValue
+
+    def read_environment(self):
+        """ Get values from environment """
+        for env in os.environ:
+            if env == "": # not sure why I get an empty string
+                continue
+            cv = self.get_configVar_obj(env)
+            cv.set_description("from envirnment")
+            cv.append(os.environ[env])
+
+    def resolve_string(self, in_str, sep=" "):
+        """ resolve a string that might contain references to values """
+        resolved_list = resolve_list((in_str,), self.resolve_value_callback)
+        retVal = sep.join(resolved_list)
+        return retVal
+
+    def resolve(self):
+        """ Resolve all values """
+        try:
+            self._resolved_vars = dict()
+            self.__resolve_stack = list()
+            for var_name in self._ConfigVar_objs:
+                if var_name in self.__resolve_stack:
+                    raise Exception("circular resolving of {}".format(value_to_resolve))
+
+                self.__resolve_stack.append(var_name)
+                self._resolved_vars[var_name] = resolve_list(self._ConfigVar_objs[var_name],
+                                                        self.resolve_value_callback)
+                self.__resolve_stack.pop()
+            self.__dirty = False
+            del self.__resolve_stack
+        except Exception as excptn:
+            print("Exception while resolving variable '"+var_name+"':", excptn)
+            raise
+
+    def resolve_value_callback(self, value_to_resolve):
+        """ callback for ConfigVar.Resolve. value_to_resolve should
+            be a single value name.
+        """
+        if value_to_resolve not in self._resolved_vars:
+            if value_to_resolve in self._ConfigVar_objs:
+                if value_to_resolve in self.__resolve_stack:
+                    raise Exception("circular resolving of {}".format(value_to_resolve))
+
+                self.__resolve_stack.append(value_to_resolve)
+                self._resolved_vars[value_to_resolve] = resolve_list(self._ConfigVar_objs[value_to_resolve],
+                                                            self.resolve_value_callback)
+                self.__resolve_stack.pop()
+        return self._resolved_vars.get(value_to_resolve, tuple())
+
+value_ref_re = re.compile("""(
+                            (?P<varref_pattern>
+                            (?P<varref_marker>[$])      # $
+                            \(                          # (
+                            (?P<var_name>\w+)           # value
+                            \))                         # )
+                            )""", re.X)
+only_one_value_ref_re = re.compile("""
+                            ^
+                            (?P<varref_pattern>
+                            (?P<varref_marker>[$])      # $
+                            \(                          # (
+                            (?P<var_name>\w+)           # value
+                            \))                         # )
+                            $
+                            """, re.X)
+
+def replace_all_from_dict(in_text, *in_replace_only_these, **in_replacement_dic):
+    """ replace all occurrences of the values in in_replace_only_these
+        with the values in in_replacement_dic. If in_replace_only_these is empty
+        use in_replacement_dic.keys() as the list of values to replace."""
+    retVal = in_text
+    if not in_replace_only_these:
+        # use the keys of of the replacement_dic as replace_only_these
+        in_replace_only_these = list(in_replacement_dic.keys())[:]
+    for look_for in in_replace_only_these:
+        retVal = retVal.replace(look_for, in_replacement_dic[look_for])
+    return retVal
+
+def resolve_list(needsResolveList, resolve_func):
+    """ resolve a list, possibly with $() style references with the help of a resolving function.
+        needsResolveList could be of type that emulates list, specifically ConfigVar.
+    """
+    replaceDic = dict()
+    resolvedList = list()
+    for valueText in needsResolveList:
+        # if the value is only a single $() reference, no quotes,
+        # the resolved values are extending the resolved list
+        single_value_ref_match = only_one_value_ref_re.match(valueText)
+        if single_value_ref_match: #
+            new_values = resolve_func(single_value_ref_match.group('var_name'))
+            resolvedList.extend(new_values)
+            continue
+        # if the value is more than a single $() reference,
+        # the resolved values are joined and appended to the resolved list
+        for value_ref_match in value_ref_re.finditer(valueText):
+            # group 'varref_pattern' is the full $(X), group 'var_name' is the X
+            replace_ref, value_ref = value_ref_match.group('varref_pattern', 'var_name')
+            if replace_ref not in replaceDic:
+                replaceDic[replace_ref] = " ".join(resolve_func(value_ref))
+        valueTextResolved = replace_all_from_dict(valueText, **replaceDic)
+        resolvedList.append(valueTextResolved)
+    if False:
+        self.__resolving_in_progress = False
+    return tuple(resolvedList)
+
+if __name__ == "__main__":
+    varList = ConfigVarList()
+    cv = varList.get_configVar_obj("itzik")
+    cv.extend( ("$(shabat)", "$(shalom)") )
+    cv.append("$(U) $(mevorach)")
+    varList.get_configVar_obj("shabat").append("saturday")
+    varList.get_configVar_obj("shalom").append("$(salam) aleykum")
+    varList.get_configVar_obj("salam").append("$(peace)")
+    varList.get_configVar_obj("peace").append("no more war")
+    varList.get_configVar_obj("mevorach").append("blessed")
+    varList.get_configVar_obj("U").append("and")
+    varList.get_configVar_obj("you").append("ata")
+
+    varList.read_environment()
+    varList.resolve()
+
+    for var in varList:
+        print (var, "=", " ".join(varList[var]), hash(var))
+
+    to_re = "on $(shabat) morning, i told $(you) and you told me $(itzik)"
+    print(varList.resolve_string(to_re))
