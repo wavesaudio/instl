d2958696980142a6b1da7892a93c6f65351a6905
Shai Shasag <shai@waves.com>
2013-12-03 17:47:55 +0200

    New version before compiling stand-alone

--- pyinstl/instlInstanceBase.py
+++ pyinstl/instlInstanceBase.py
@@ -25,7 +25,7 @@ os_second_name = current_os_names[0]
 if len(current_os_names) > 1:
     os_second_name = current_os_names[1]
 
-INSTL_VERSION=(0, 4, 0)
+INSTL_VERSION=(0, 5, 0)
 this_program_name = "instl"
 
 class InstallInstructionsState(object):
