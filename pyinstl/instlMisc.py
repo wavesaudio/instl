bbc58f5fc47f0259ed2b3956e07fe3395d694b3f
Shai Shasag <shai@waves.com>
2014-02-13 18:30:58 +0200

    * New command parallel-run to help run curl downloads in parallel.

--- pyinstl/instlMisc.py
+++ pyinstl/instlMisc.py
@@ -2,9 +2,9 @@
 
 from __future__ import print_function
 
-from pyinstl.log_utils import func_log_wrapper
-from pyinstl.utils import *
+import shlex
 
+from pyinstl.utils import *
 from instlInstanceBase import InstlInstanceBase
 from pyinstl import svnTree
 
@@ -17,11 +17,9 @@ class InstlMisc(InstlInstanceBase):
 
     def do_command(self):
         the_command = self.cvl.get_str("__MAIN_COMMAND__")
-        if the_command in self.cvl.get_list("__ALLOWED_COMMANDS__"):
-            do_command_func = getattr(self, "do_"+the_command)
-            do_command_func()
-        else:
-            raise ValueError(the_command+" is not one of the allowed admin commands: "+" ".join(self.cvl.get_list("__ALLOWED_COMMANDS__")))
+        fixed_command = the_command.replace('-', '_')
+        do_command_func = getattr(self, "do_"+fixed_command)
+        do_command_func()
 
     def do_version(self):
         print(self.get_version_str())
@@ -30,3 +28,15 @@ class InstlMisc(InstlInstanceBase):
         import pyinstl.helpHelper
         help_folder_path = os.path.join(self.cvl.resolve_string("$(__INSTL_DATA_FOLDER__)"), "help")
         pyinstl.helpHelper.do_help(self.cvl.get_str("__HELP_SUBJECT__"), help_folder_path)
+
+    def do_parallel_run(self):
+        processes_list_file = self.cvl.get_str("__MAIN_INPUT_FILE__")
+        commands = list()
+        with open(processes_list_file, "r") as rfd:
+            for line in rfd:
+                line = line.strip()
+                if line and line[0] != "#":
+                    args = shlex.split(line)
+                    commands.append(args)
+        from parallel_run import run_processes_in_parallel
+        run_processes_in_parallel(commands)
---
+++ pyinstl/parallel_run.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python2.7
+from __future__ import print_function
+
+import subprocess
+import time
+import sys
+import signal
+import shlex
+from collections import deque
+
+exit_val = 0
+proc_que = deque()
+
+def run_processes_in_parallel(commands):
+    global exit_val
+    try:
+        install_signal_handlers()
+        run_parallels(commands)
+        exit_val = 0
+        killall_and_exit()
+    except Exception as es:
+        killall_and_exit()
+
+def run_parallels(commands):
+    global exit_val
+    retVal = 0
+    max_time = 6
+    start_time = time.time()
+    for command in commands:
+        try:
+            proc = subprocess.Popen(command, executable=command[0], shell=False)#, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            #print("Started", command, proc.pid)
+            sys.stdout.flush()
+            proc_que.append(proc)
+        except Exception as es:
+            print("failed to start", command, es.strerror, file=sys.stderr)
+            sys.stdout.flush()
+            exit_val = es.strerror
+            raise
+            
+    while proc_que:
+        proc = proc_que.popleft()
+        status = proc.poll()
+        if status is None: # None means it's still alive
+            #print(proc.pid, "still alive")
+            proc_que.append(proc)
+        else:
+            #print(proc.pid, "just died", status)
+            if status != 0:
+                exit_val = status
+                killall_and_exit()
+        sys.stdout.flush()
+        time.sleep(.5)
+   
+def signal_handler(signum, frame):
+    global exit_val
+    #print("signal", signum, frame)
+    exit_val = signum
+    killall_and_exit()
+
+def killall_and_exit():
+    for proc in proc_que:
+        proc.kill()
+    exit(exit_val)
+    
+def install_signal_handlers():
+    for sig in (signal.SIGABRT, signal.SIGFPE, signal.SIGILL, signal.SIGINT, signal.SIGSEGV, signal.SIGTERM):
+        signal.signal(sig, signal_handler)
