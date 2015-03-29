2fd4a8ea205fe4aa08ab2fb540b8d2e6c1278bce
shai <shaishasag@yahoo.co.uk>
2015-03-29 20:50:42 +0300

    * Replaced default value path_so_far=list() with path_so_far=None * fixed wrong parameter name on CopyTool_win_xcopy.copy_dir_files_to_dir

--- pyinstl/instlInstanceSync_url.py
+++ pyinstl/instlInstanceSync_url.py
@@ -269,7 +269,9 @@ class InstlInstanceSync_url(InstlInstanceSync):
 
         self.instlObj.batch_accum += self.instlObj.platform_helper.popd()
 
-    def create_prefix_instructions_for_item(self, accum, item, path_so_far=list()):
+    def create_prefix_instructions_for_item(self, accum, item, path_so_far=None):
+        if path_so_far is None:
+            path_so_far = list()
         if item.isSymlink():
             print("Found symlink at", item.full_path())
         elif item.isFile():
@@ -284,7 +286,9 @@ class InstlInstanceSync_url(InstlInstanceSync):
             #path_so_far.pop()
 
 
-    def create_download_instructions_for_item(self, item, path_so_far=list()):
+    def create_download_instructions_for_item(self, item, path_so_far=None):
+        if path_so_far is None:
+            path_so_far = list()
         if item.isSymlink():
             print("Found symlink at", item.full_path())
         elif item.isFile():
