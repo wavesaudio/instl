9a7afb4d9fcce5c2eabf7b0405f488a244063cd5
shai <shais@waves.com>
2016-09-27 19:55:18 +0300

    * resolve_inheritance works (some what...)

--- pyinstl/itemRow.py
+++ pyinstl/itemRow.py
@@ -8,16 +8,20 @@ alchemy_base = declarative_base()
 
 class ItemRow(alchemy_base):
     __tablename__ = 'ItemRow'
+    #row_id = Column(Integer, primary_key=True, autoincrement=True)
     iid = Column(String, primary_key=True)
     name = Column(String(128), default=None)
+    inheritance_resolved = Column(BOOLEAN, default=False)
 
     def __repr__(self):
+        resolved_str = "resolved" if self.inheritance_resolved else "unresolved"
         return ("<{self.iid}"
-                ", name:{self.name}>"
+                ", name:{self.name}, inheritance {resolved}>"
                 ).format(**locals())
 
     def __str__(self):
-        retVal = "{self.iid}: '{self.name}'".format(**locals())
+        resolved_str = "resolved" if self.inheritance_resolved else "unresolved"
+        retVal = "{self.iid}: '{self.name}', inheritance {resolved_str}".format(**locals())
         return retVal
 
 
@@ -38,6 +42,6 @@ class ItemDetailRow(alchemy_base):
                 ).format(**locals())
 
     def __str__(self):
-        retVal = "{self.detail_id}, {self.iid}, {self.os}, {self.detail_name}, {self.detail_value}, {self.inherited}".format(**locals())
+        retVal = "{self.detail_id}, {self.iid}, {self.os}, {self.detail_name}: {self.detail_value}, inherited: {self.inherited}".format(**locals())
         return retVal
 
