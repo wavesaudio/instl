94e6dde10d7f65848e7cab918533268fa7687475
GuyCatz <guy.catz@waves.com>
2016-12-22 07:05:21 +0200

    Revert "Revert "* * MultiFileReader: new class to read parts of wtar files""
    This reverts commit d72304fea732bad99638490ac3132bf9a9babed2.

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,6 +2,7 @@ from .utils import *
 from .searchPaths import SearchPaths
 from .instlException import InstlException, InstlFatalException
 from .parallel_run import run_processes_in_parallel
+from .multi_file import MultiFileReader
 import platform
 current_os = platform.system()
 if current_os == 'Darwin':
