bd5595dd4c29c25f79d428563d6a9caed68a5b68
Shai Shasag <shai@waves.com>
2017-10-01 16:08:49 +0300

    * fixed and issue with exec command that did not initialize items_table * removed sme redundant code from YamlReader.read_yaml_file instl 1.6.2.6

--- aYaml/yamlReader.py
+++ aYaml/yamlReader.py
@@ -51,15 +51,6 @@ class YamlReader(object):
             buffer = io.StringIO(buffer)     # turn text to a stream
             kwargs['path-to-file'] = file_path
             self.read_yaml_from_stream(buffer, *args, **kwargs)
-            if False:
-                with utils.open_for_read_file_or_url(file_path, self.url_translator, self.path_searcher) as open_file:
-                    self.file_read_stack[-1] = open_file.actual_path
-                    buffer = open_file.fd.read()
-                    buffer = utils.unicodify(buffer) # make sure text is unicode
-                    buffer = io.StringIO(buffer)     # turn text to a stream
-                    buffer.name = open_file.actual_path          # this will help identify the file for debugging and messages
-                    kwargs['path-to-file'] = open_file.actual_path
-                    self.read_yaml_from_stream(buffer, *args, **kwargs)
             self.file_read_stack.pop()
         except (FileNotFoundError, urllib.error.URLError) as ex:
             ignore = kwargs.get('ignore_if_not_exist', False)
