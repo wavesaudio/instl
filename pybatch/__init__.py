9c1982c3dca318823851759da6d9a67ab381d97d
shai <shai@waves.com>
2018-08-05 19:46:15 +0300

    * moved python batch commands that do reporting to their own file reportingBatchCommands.py

--- pybatch/__init__.py
+++ pybatch/__init__.py
@@ -1,13 +1,19 @@
 import sys
 
 from .batchCommands import PythonBatchCommandBase
-from .batchCommandAccum import PythonBatchCommandAccum
 from .batchCommandAccum import batch_repr
+from .batchCommandAccum import PythonBatchCommandAccum
 
 from .copyBatchCommands import CopyDirContentsToDir
 from .copyBatchCommands import CopyDirToDir
 from .copyBatchCommands import CopyFileToDir
 from .copyBatchCommands import CopyFileToFile
+
+from .reportingBatchCommands import Echo
+from .reportingBatchCommands import Progress
+from .reportingBatchCommands import Remark
+from .reportingBatchCommands import Section
+from .reportingBatchCommands import VarAssign
 
 from .batchCommands import AppendFileToFile
 from .batchCommands import Cd
@@ -21,13 +27,11 @@ from .batchCommands import RmDir
 from .batchCommands import RmFile
 from .batchCommands import RmFileOrDir
 from .batchCommands import RunProcessBase
-from .batchCommands import Section
-from .batchCommands import SingleShellCommand
 from .batchCommands import ShellCommands
+from .batchCommands import SingleShellCommand
 from .batchCommands import touch
 from .batchCommands import Touch
 from .batchCommands import Unlock
-from .batchCommands import VarAssign
 
 from .wtarBatchCommands import Wtar, Unwtar, Wzip, Unwzip
 
@@ -37,11 +41,11 @@ if sys.platform == "win32":
     from .batchCommandsWinOnly import WinShortcut
 
 if sys.platform == "darwin":
-    from .batchCommandsMacOnly import MacDock
     from .batchCommandsMacOnly import CreateSymlink
-    from .batchCommandsMacOnly import SymlinkToSymlinkFile
-    from .batchCommandsMacOnly import SymlinkFileToSymlink
     from .batchCommandsMacOnly import CreateSymlinkFilesInFolder
+    from .batchCommandsMacOnly import MacDock
     from .batchCommandsMacOnly import ResolveSymlinkFilesInFolder
+    from .batchCommandsMacOnly import SymlinkFileToSymlink
+    from .batchCommandsMacOnly import SymlinkToSymlinkFile
 
 from .new_batchCommands import *
