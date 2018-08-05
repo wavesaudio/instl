bc80151418ccef368577a94c4452af91ee6d0717
shai <shai@waves.com>
2018-08-05 11:44:09 +0300

    * python batch commands dealing with copy moved to their own file copyBatchCommands.py

--- pybatch/__init__.py
+++ pybatch/__init__.py
@@ -4,15 +4,16 @@ from .batchCommands import PythonBatchCommandBase
 from .batchCommandAccum import PythonBatchCommandAccum
 from .batchCommandAccum import batch_repr
 
+from .copyBatchCommands import CopyDirContentsToDir
+from .copyBatchCommands import CopyDirToDir
+from .copyBatchCommands import CopyFileToDir
+from .copyBatchCommands import CopyFileToFile
+
 from .batchCommands import AppendFileToFile
 from .batchCommands import Cd
 from .batchCommands import ChFlags
 from .batchCommands import Chmod
 from .batchCommands import Chown
-from .batchCommands import CopyDirContentsToDir
-from .batchCommands import CopyDirToDir
-from .batchCommands import CopyFileToDir
-from .batchCommands import CopyFileToFile
 from .batchCommands import MakeDirs
 from .batchCommands import MakeRandomDirs
 from .batchCommands import ParallelRun
@@ -22,6 +23,7 @@ from .batchCommands import RmFileOrDir
 from .batchCommands import RunProcessBase
 from .batchCommands import Section
 from .batchCommands import SingleShellCommand
+from .batchCommands import ShellCommands
 from .batchCommands import touch
 from .batchCommands import Touch
 from .batchCommands import Unlock
