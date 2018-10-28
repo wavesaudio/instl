6b81ed3bcb102dda4b98e15390a5e2669ee92c65
shai <shai@waves.com>
2018-10-28 13:13:15 +0200

    * started implementing SVNLastRepoRev

--- pybatch/svnBatchCommands.py
+++ pybatch/svnBatchCommands.py
@@ -5,13 +5,12 @@ from .subprocessBatchCommands import RunProcessBase
 
 
 class SVNClient(RunProcessBase):
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, command, *args, **kwargs) -> None:
         super().__init__(**kwargs)
-        self.svn_args = args
+        self.command = command
 
     def repr_own_args(self, all_args: List[str]) -> None:
-        for arg in self.svn_args:
-            all_args.append(utils.quoteme_raw_if_string(arg))
+        all_args.append(self.command)
 
     def progress_msg_self(self) -> str:
         return f''''''
@@ -19,4 +18,20 @@ class SVNClient(RunProcessBase):
     def create_run_args(self):
         svn_client = config_vars.get("SVN_CLIENT_PATH", "svn").str()
         run_args = [svn_client] + list(self.svn_args)
+        return run_args
+
+
+class SVNLastRepoRev(SVNClient):
+    def __init__(self, url_param, reply_param, *args, **kwargs):
+        self.url_param = url_param
+        self.reply_param = reply_param
+
+    def create_run_args(self):
+        svn_client = config_vars.get("SVN_CLIENT_PATH", "svn").str()
+        command = "info"
+        url = config_vars[self.url_param]
+        run_args = list()
+        run_args.append(svn_client)
+        run_args.append(command)
+        run_args.append(url)
         return run_args
