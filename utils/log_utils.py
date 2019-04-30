e3ebe2f79ab70be54a66fdf1500cc51f391bde7a
Nir Arad <nira@waves.com>
2019-04-30 09:59:22 +0300

    Fixed enqueue_output() - releasing buffers when a new line is detected run_processes_in_parallel() - Added install_mp_handler() to avoid multithread permission errors when rotating log files.

--- utils/log_utils.py
+++ utils/log_utils.py
@@ -11,9 +11,11 @@ import os
 import re
 import sys
 import json
-import appdirs
 import inspect
-import pathlib
+import multiprocessing
+import threading
+import traceback
+import queue
 import logging
 import logging.handlers
 
@@ -207,3 +209,107 @@ def remove_log_handler(handler_to_remove_name):
         if handler.name == handler_to_remove_name:
             print(f"remove log handler {handler.name}")
             the_logger.removeHandler(handler)
+
+
+def close_log_hdlrs(hdlr_cls=logging.FileHandler, hdlr_name=None):
+    '''Walks through all logging handlers (based on the requested handler class type), and closes them
+       The default type is FileHandlerPlus'''
+    root_logger = logging.getLogger()
+    hdlrs_to_close = sorted(get_hdlrs(hdlr_cls=hdlr_cls, hdlr_name=hdlr_name), key=lambda hdlr: str(hdlr) == 'DEBUG')  # Placing debug handler at the end
+    for hdlr in hdlrs_to_close:
+        root_logger.removeHandler(hdlr)
+        try:
+            hdlr.close()
+        except OSError:
+            root_logger.warning('Failed to close log handler - %s' % hdlr)
+
+
+def install_mp_handler(logger=None):
+    """Wraps the handlers in the given Logger with an MultiProcessingHandler.
+
+    :param logger: whose handlers to wrap. By default, the root logger.
+    """
+    if logger is None:
+        logger = logging.getLogger()
+
+    for i, orig_handler in enumerate(list(logger.handlers)):
+        handler = MultiProcessingHandler(
+            'mp-handler-{0}'.format(i), sub_handler=orig_handler)
+
+        logger.removeHandler(orig_handler)
+        logger.addHandler(handler)
+
+
+class MultiProcessingHandler(logging.Handler):
+
+    def __init__(self, name, sub_handler=None):
+        super(MultiProcessingHandler, self).__init__()
+
+        if sub_handler is None:
+            sub_handler = logging.StreamHandler()
+        self.sub_handler = sub_handler
+
+        self.setLevel(self.sub_handler.level)
+        self.setFormatter(self.sub_handler.formatter)
+
+        self.queue = multiprocessing.Queue(-1)
+        self._is_closed = False
+        # The thread handles receiving records asynchronously.
+        self._receive_thread = threading.Thread(target=self._receive, name=name)
+        self._receive_thread.daemon = True
+        self._receive_thread.start()
+
+    def setFormatter(self, fmt):
+        super(MultiProcessingHandler, self).setFormatter(fmt)
+        self.sub_handler.setFormatter(fmt)
+
+    def _receive(self):
+        while not (self._is_closed and self.queue.empty()):
+            try:
+                record = self.queue.get(timeout=0.2)
+                self.sub_handler.emit(record)
+            except (KeyboardInterrupt, SystemExit):
+                raise
+            except EOFError:
+                break
+            except queue.Empty:
+                pass  # This periodically checks if the logger is closed.
+            except:
+                traceback.print_exc(file=sys.stderr)
+
+        self.queue.close()
+        self.queue.join_thread()
+
+    def _send(self, s):
+        self.queue.put_nowait(s)
+
+    def _format_record(self, record):
+        # ensure that exc_info and args
+        # have been stringified. Removes any chance of
+        # unpickleable things inside and possibly reduces
+        # message size sent over the pipe.
+        if record.args:
+            record.msg = record.msg % record.args
+            record.args = None
+        if record.exc_info:
+            self.format(record)
+            record.exc_info = None
+
+        return record
+
+    def emit(self, record):
+        try:
+            s = self._format_record(record)
+            self._send(s)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
+
+    def close(self):
+        if not self._is_closed:
+            self._is_closed = True
+            self._receive_thread.join(5.0)  # Waits for receive queue to empty.
+
+            self.sub_handler.close()
+            super(MultiProcessingHandler, self).close()
