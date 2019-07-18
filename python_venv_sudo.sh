1e93dabac607fdc5ecf7887b1aadbe3ac6c4cd4a
shai <shai@waves.com>
2019-07-18 19:36:07 +0300

    * script to server as interpreter that runs under sudo, so PyCharm can debug under sudo

---
+++ python_venv_sudo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT="${BASH_SOURCE[0]}"
+SCRIPT_FOLDER=$(dirname $SCRIPT)
+
+#export PYTHONPATH=${SCRIPT_FOLDER}:${SCRIPT_FOLDER}/pybatch:${SCRIPT_FOLDER}/pybatch/test:${PYTHONPATH}
+
+echo ${PYTHONPATH}
+
+source "${SCRIPT_FOLDER}/venv/bin/activate"
+sudo python3.6 "$@"
