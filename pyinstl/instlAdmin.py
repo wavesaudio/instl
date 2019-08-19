3e9372e55eca768c606e0fec3d9a8e14695afece
shai <shai@waves.com>
2019-08-19 15:08:39 +0300

    * new python-batch RmDirContents removes contents f dir but not the dir itself * instlAdmin.up2s3_repo_rev will clean the work folder using RmDirContents

--- pyinstl/instlAdmin.py
+++ pyinstl/instlAdmin.py
@@ -1246,6 +1246,7 @@ class InstlAdmin(InstlInstanceBase):
                 sub_accum += Subprocess("aws", "s3", "sync", os.curdir, "s3://$(S3_BUCKET_NAME)/$(REPO_NAME)/$(__CURR_REPO_FOLDER_HIERARCHY__)", "--exclude", "*.DS_Store")
                 repo_rev_file_path = config_vars["UPLOAD_REVISION_REPO_REV_FILE"].str()
                 sub_accum += Subprocess("aws", "s3", "cp", repo_rev_file_path, "s3://$(S3_BUCKET_NAME)/admin/", "--content-type", 'text/plain')
+            batch_accum += RmDirContents(revision_folder_path, exclude='instl')
 
             self.write_batch_file(batch_accum)
             if bool(config_vars["__RUN_BATCH__"]):
