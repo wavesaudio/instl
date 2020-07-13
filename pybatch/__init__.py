bc2b7b32ce6aa96f56814edce834a7bde69e6332
Oren Cohen <orenc@orenc-hsiembp.lan>
2020-07-13 15:38:03 +0300

    added dummy classes for test purposes (prevent false positives/negatives)

--- pybatch/__init__.py
+++ pybatch/__init__.py
@@ -128,6 +128,7 @@ if sys.platform == "darwin":
     from .MacOnlyBatchCommands import SymlinkFileToSymlink
     from .MacOnlyBatchCommands import SymlinkToSymlinkFile
 
+    #Added for test purposes, without those classes verify_actions gives false positives/negatives
     class PythonBatchCommandDummy(PythonBatchCommandBase):
         def __init__(self, *args, **kwargs) -> None:
             pass
@@ -141,6 +142,22 @@ if sys.platform == "darwin":
 
     class CreateRegistryValues(PythonBatchCommandDummy):
         pass
+
+    class ReadRegistryValue(PythonBatchCommandDummy):
+        pass
+
+    class CreateRegistryKey(PythonBatchCommandDummy):
+        pass
+
+    class DeleteRegistryKey(PythonBatchCommandDummy):
+        pass
+
+    class DeleteRegistryValues(PythonBatchCommandDummy):
+        pass
+
+    class ResHackerCompileResource(PythonBatchCommandDummy):#??
+        pass
+
 
 from .new_batchCommands import *
 
@@ -165,3 +182,4 @@ def EvalShellCommand(action_str: str, message: str, python_batch_names=None, rai
                     log.warning(f"""'{action_str}' was evaled as ShellCommand not as python batch""")
 
     return retVal
+
