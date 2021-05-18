61a6ca0a4a2dd0df613ef37b06fa84bdb54d06bc
idanwork <idanmz@waves.com>
2021-05-18 10:45:47 +0300

    chown after unwtar UTT (IdanMZ)

--- pyinstl/instlClientCopy.py
+++ pyinstl/instlClientCopy.py
@@ -248,6 +248,10 @@ class InstlClientCopy(InstlClient):
 
             if len(wtar_base_names) > 0:
                 retVal += Unwtar(source_path_abs, os.curdir)
+                log.warning(f"Experimental chown {os.curdir} - does not work as intended, it changes the inner files and not the bunlde")
+                retVal += Chown(path=Path(os.curdir),
+                              user_id=int(config_vars.get("ACTING_UID", -1)),
+                              group_id=int(config_vars.get("ACTING_GID", -1)), recursive=True)
         else:
             # it might be a dir that was wtarred
             retVal = self.create_copy_instructions_for_file(source_path, name_for_progress_message)
