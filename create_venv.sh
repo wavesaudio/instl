b70f7065fb0ed6743268f5cd812d3e3a2fbfe193
shai <shai@waves.com>
2021-10-20 12:59:42 +0300

    * added requirements_mac_only.txt to create_venv.sh

--- create_venv.sh
+++ create_venv.sh
@@ -3,4 +3,5 @@
 python3.9 -m venv venv
 source venv/bin/activate
 python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements_mac_only.txt
 python3.9 -m pip install -r requirements_admin.txt
