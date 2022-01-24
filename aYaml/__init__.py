75244dea5b8c96d442154fdd8d2fe72172614db3
shai <shai@waves.com>
2022-01-24 09:54:04 +0200

    Revert "* switched back to augmentedYaml, strictyaml wouldn't cut it"
    This reverts commit 826e646c05cb9e14f0f1e230546832c1eb442378.

--- aYaml/__init__.py
+++ aYaml/__init__.py
@@ -4,5 +4,5 @@
     Licensed under BSD 3 clause license, see LICENSE file for details.
 """
 
-from .augmentedYaml import YamlDumpDocWrap, YamlDumpWrap, writeAsYaml, nodeToPy
+from .augmentedYaml import YamlDumpDocWrap, YamlDumpWrap, writeAsYaml
 from .yamlReader import YamlReader
