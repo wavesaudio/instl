783eea1abb14bb0f9566538eab2bdc1584b9edb3
shai <shai@waves.com>
2022-01-31 16:45:05 +0200

    * removed requirements_admin.txt from create_venv.sh because it will make compiled instl bigger with modules that are not needed for users.

--- create_venv.sh
+++ create_venv.sh
@@ -5,4 +5,3 @@ source venv/bin/activate
 python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 python3.9 -m pip install -r requirements_mac_only.txt
-python3.9 -m pip install -r requirements_admin.txt
