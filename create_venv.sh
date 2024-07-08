b8aca59b7c6ca40d37603f3bba87ee0a18cc7aaa
ShaiWavesAudio <shai@waves.com>
2024-07-08 16:37:48 +0300

    * pip install mac requirements first, so modules installed with --no-binary will be correct

--- create_venv.sh
+++ create_venv.sh
@@ -3,5 +3,5 @@
 python3.12 -m venv venv
 source venv/bin/activate
 python3.12 -m pip install --upgrade pip
-python3.12 -m pip install -r requirements.txt
 python3.12 -m pip install -r requirements_mac_only.txt
+python3.12 -m pip install -r requirements.txt
