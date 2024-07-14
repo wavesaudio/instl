8d4e8be41a8cd42cb8050182cc50f960a97dd627
ShaiShasag <shaishasag@yahoo.co.uk>
2024-07-14 14:34:50 +0300

    * more elaborate create_venv.sh

--- create_venv.sh
+++ create_venv.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+echo ---* creating virtual env in $(pwd) *---
+
 python3.12 -m venv venv
 source venv/bin/activate
+
+echo ---* pip installing *---
+
 python3.12 -m pip install --upgrade pip
 # pip install mac requirements first, so universal binaries will get priority
 python3.12 -m pip install -r requirements_mac_only.txt
 python3.12 -m pip install -r requirements.txt
+
+echo ---* creating virtual env done *---
