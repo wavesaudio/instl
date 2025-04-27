e65f4818df48d48e3549ea9bd64f40f25184adb0
ShaiShasag <shaishasag@yahoo.co.uk>
2025-04-27 10:33:39 +0300

    No distutils (#82)
    * * removed dependency on distutils and setuptools as much as possible
    instl 2.5.1.6
    
    * Syntax error fix
    
    * Substitute StrictVersion with packaging.Version
    
    ---------
    
    Co-authored-by: shai <shai@waves.com>
    Co-authored-by: eladstr <95563185+eladstr@users.noreply.github.com>

--- pyinstl/curlHelper.py
+++ pyinstl/curlHelper.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3.12
 
-
-import os
-import abc
-import itertools
 import subprocess
 from pathlib import Path, PurePath
 import sys
-from distutils.version import StrictVersion
 import functools
 import logging
 import re
+from packaging.version import Version
 
 log = logging.getLogger()
 if sys.platform == 'win32':
@@ -112,8 +108,8 @@ parallel-max = {max_parallel_downloads}
                 match = re.search(r"curl\s+([0-9.]+)\s", proc.stdout.read())
 
                 if match is not None and len(match.groups()) > 0:
-                    curl_version = StrictVersion(match.group(1))
-                    min_version = StrictVersion(CUrlHelper.min_supported_parallel_curl_version)
+                    curl_version = Version(match.group(1))
+                    min_version = Version(CUrlHelper.min_supported_parallel_curl_version)
                     if min_version > curl_version:
                         log.info(f"Detected a legacy curl version {match.group(1)}")
                     else:
