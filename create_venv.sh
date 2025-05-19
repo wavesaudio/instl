13d8b56d444a44b20cc1766eceeec5c125a9a8cc
ShaiWavesAudio <shai@waves.com>
2025-05-19 15:48:44 +0300

    * in create_venv.sh made sure all pip installations are --on-user, add some messages instl 2.5.1.6

--- create_venv.sh
+++ create_venv.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-echo ---* creating virtual env in: $(pwd) *---
 echo ---* python is: $(which python3.12) *---
+python3.12 -m pip install --upgrade pip
+
+echo ---* creating virtual env in: $(pwd)/venv *---
 
 python3.12 -m venv venv
 source venv/bin/activate
+echo ---* after "source venv/bin/activate" VIRTUAL_ENV is:
+echo    $VIRTUAL_ENV  *---
+
+echo ---* activated virtual env in: $(pwd)/venv *---
+echo ---* now python is: $(which python3.12) *---
+
+python3.12 -m pip install pip  --no-user
 
 echo ---* pip installing *---
 
-python3.12 -m pip install --upgrade pip
 # pip install mac requirements first, so universal binaries will get priority
-python3.12 -m pip install -r requirements_mac_only.txt
-python3.12 -m pip install -r requirements.txt
+python3.12 -m pip install --upgrade pip  --no-user
+python3.12 -m pip install -r requirements_mac_only.txt  --no-user
+python3.12 -m pip install -r requirements.txt  --no-user
 
 echo ---* creating virtual env done *---
